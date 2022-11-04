cc25d5b4347fc3a85afb5ea88eaf2f5992e7c28e
kautsar07 <kautsar07777@gmail.com>
2022-11-04 20:18:27 +0700

    add env api

--- src/component/features/auth/authSlice.js
+++ src/component/features/auth/authSlice.js
@@ -17,16 +17,15 @@ import {
   where,
   addDoc,
 } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCkhA3vVtuj4oecL4i6iXFbjuMZkUgqEx0",
-  authDomain: "moviedb-365812.firebaseapp.com",
-  projectId: "moviedb-365812",
-  storageBucket: "moviedb-365812.appspot.com",
-  messagingSenderId: "202764783206",
-  appId: "1:202764783206:web:9f4baa2a64168c47b467a7",
-  measurementId: "G-XHR9SV53ZF",
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
